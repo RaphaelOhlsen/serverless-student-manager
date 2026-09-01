8e6e7a102bd802e92d8723771b7dd6e3d371b50b
Raphael Bernardo Ohlsen <seu-email@exemplo.com>
2026-09-01 08:55:41 -0300

    feat(frontend): initialize React application

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly VITE_COGNITO_USER_POOL_ID: string
+  readonly VITE_COGNITO_USER_POOL_CLIENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
